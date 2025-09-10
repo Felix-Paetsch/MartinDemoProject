f668e3a37d3ba3abafa175834321996a340fb7da
Felix <felixpaetsch@gmail.com>
2025-09-10 11:50:22 +0200

    NVIM stuff

--- dev/lib/src/messaging/base/address.ts
+++ dev/lib/src/messaging/base/address.ts
@@ -1,42 +1,55 @@
-import { Context, Data, Effect, Equal, Hash, ParseResult, Schema } from "effect";
+import { Context, Data, Effect, Equal, Hash, Schema } from "effect";
 import { v4 as uuidv4 } from 'uuid';
+import { AddressDeserializationError } from "./errors/anomalies"
 
-export class AddressDeserializationError extends Data.TaggedError("AddressDeserializationError")<{
-    address: unknown;
-}> { }
 export class AddressT extends Context.Tag("AddressT")<AddressT, Address>() { }
 
-export type SerializedAddress = `primary_id: ${string}\nsecondary_id: ${string}`
+export type ProcessID = string
+export type Port = string
+
+const SerializedAddressSchema = Schema.Struct({
+    process_id: Schema.String,
+    port: Schema.String
+})
+
+export type SerializedAddress = {
+    process_id: ProcessID,
+    port: Port
+}
 
 export class Address implements Equal.Equal {
-    readonly _primary_id: string;
-    readonly _secondary_id: string;
+    readonly _process_id: ProcessID;
+    readonly _port: Port;
 
     constructor(
-        primary_id: string = uuidv4(),
-        secondary_id: string = uuidv4()
+        process_id: ProcessID = uuidv4(),
+        port: Port = uuidv4()
     ) {
-        this._primary_id = primary_id;
-        this._secondary_id = secondary_id;
+        this._process_id = process_id;
+        this._port = port;
     }
 
-    get primary_id(): string {
-        return this._primary_id;
+    get process_id(): string {
+        return this._process_id;
     }
 
-    get secondary_id(): string {
-        return this._secondary_id;
+    get port(): string {
+        return this._port;
     }
 
     as_generic(): Address {
-        return new Address(this.primary_id, "*");
+        return new Address(this.process_id, "*");
+    }
+
+    static generic(process_id: ProcessID): Address {
+        return new Address(process_id, "*");
     }
 
     [Equal.symbol](that: Equal.Equal): boolean {
         if (that instanceof Address) {
             return (
-                Equal.equals(this.primary_id, that.primary_id) &&
-                Equal.equals(this.secondary_id, that.secondary_id)
+                Equal.equals(this.process_id, that.process_id) &&
+                Equal.equals(this.port, that.port)
             )
         }
 
@@ -44,56 +57,32 @@ export class Address implements Equal.Equal {
     }
 
     [Hash.symbol](): number {
-        return Hash.hash(this.secondary_id)
+        return Hash.hash(this.port)
     }
 
     serialize(): SerializedAddress {
-        const serialized: string = Schema.encodeSync(Address.AddressFromString)(this);
-        return serialized as SerializedAddress;
-    }
-
-    toString(): string {
-        return this.serialize();
+        return Schema.encodeSync(SerializedAddressSchema)(this);
     }
 
     static deserialize(serialized: SerializedAddress): Address {
-        return Address.deserializeFromUnkownE(serialized).pipe(
+        return Address.deserializeE(serialized).pipe(
             Effect.runSync
         );
     }
 
-    static deserializeE(serialized: SerializedAddress): Effect.Effect<Address, AddressDeserializationError> {
-        return Schema.decode(Address.AddressFromString)(serialized)
-            .pipe(
-                Effect.catchTag("ParseError", () => new AddressDeserializationError({ address: serialized }))
-            )
+    static deserializeE(serialized: unknown): Effect.Effect<Address, AddressDeserializationError> {
+        return Effect.gen(function* () {
+            const json = yield* Schema.decodeUnknown(SerializedAddressSchema)(serialized);
+            return new Address(json.process_id, json.port);
+        }).pipe(
+            Effect.mapError(() => new AddressDeserializationError({ address: serialized }))
+        )
     }
-
-    static deserializeFromUnkownE(serialized: unknown): Effect.Effect<Address, AddressDeserializationError> {
-        return Schema.decodeUnknown(Address.AddressFromString)(serialized)
-            .pipe(
-                Effect.catchTag("ParseError", () => new AddressDeserializationError({ address: serialized }))
-            )
-    }
-
-    static AddressFromString = Schema.transformOrFail(Schema.String, Schema.instanceOf(Address), {
-        decode: (str: string, _, ast) => {
-            const lines = str.split("\n");
-            const primary_id = lines[0]?.split(": ")[1] ?? "";
-            const secondary_id = lines[1]?.split(": ")[1] ?? "";
-            if (!primary_id || !secondary_id) {
-                return ParseResult.fail(new ParseResult.Type(ast, str, "Failed to deserialize address"));
-            }
-            return Effect.succeed(new Address(primary_id, secondary_id));
-        },
-        encode: (address: Address) =>
-            ParseResult.succeed(`primary_id: ${address.primary_id}\nsecondary_id: ${address.secondary_id}`)
-    })
 
     private static _local_address: Address = new Address(uuidv4(), uuidv4());
     static setLocalAddress(address: Address) {
         if (address instanceof LocalAddress) {
-            this._local_address = new Address(address.primary_id, address.secondary_id);
+            this._local_address = new Address(address.process_id, address.port);
             return;
         }
         this._local_address = address;
@@ -103,19 +92,19 @@ export class Address implements Equal.Equal {
         return this._local_address;
     }
 
-    static new_local_address = (secondary_id: string = uuidv4()) => {
-        return new LocalAddress(secondary_id);
+    static new_local_address = (port: string = uuidv4()) => {
+        return new LocalAddress(port);
     }
 }
 
 export class LocalAddress extends Address {
     constructor(
-        secondary_id: string = uuidv4()
+        port: string = uuidv4()
     ) {
-        super(Address.local_address.primary_id, secondary_id)
+        super(Address.local_address.process_id, port)
     }
 
-    get primary_id() {
-        return Address.local_address.primary_id
+    get process_id() {
+        return Address.local_address.process_id
     }
 }
--- dev/lib/src/messaging/base/errors/anomalies.ts
+++ dev/lib/src/messaging/base/errors/anomalies.ts
@@ -3,23 +3,29 @@ import { Address } from "../address";
 import { Message } from "../message";
 
 export class AddressNotFound extends Error {
-    constructor(readonly address: Address) {
-        super(`Address: '${address.toString()}' not found`, {
-            cause: address
-        });
-    }
+        constructor(readonly address: Address) {
+                super(`Address: '${address.toString()}' not found`, {
+                        cause: address
+                });
+        }
 }
 
 export class MessageSerializationError extends Data.TaggedError("MessageSerializationError")<{
-    message: Message
+        message: Message
 }> { }
 
 export class MessageDeserializationError extends Data.TaggedError("MessageDeserializationError")<{}> { }
 
 export class MessageChannelTransmissionError extends Error {
-    constructor(readonly error: Error) {
-        super("Message Channel Transmission Error: " + error.message, { cause: error });
-    }
+        constructor(readonly error: Error) {
+                super("Message Channel Transmission Error: " + error.message, { cause: error });
+        }
+}
+
+export class AddressDeserializationError extends Error {
+        constructor(readonly address: any) {
+                super("Address not deserializable");
+        }
 }
 
 export type Anomaly = AddressNotFound | MessageSerializationError | MessageDeserializationError | MessageChannelTransmissionError;
